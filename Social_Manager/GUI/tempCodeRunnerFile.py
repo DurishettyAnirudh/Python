a36777fca7bb8b34f8f91e450f5330297a01dd0d
ghost <danirudh100@gmail.com>
2025-07-24 20:01:56 +0530

    removed social manger

--- Social_Manager/GUI/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-  # #for Youtube Upload
-        # print("python", "./Youtube/upload_video.py", video_path)
-        # subprocess.Popen(["python", "./Youtube/upload_video.py", "--file" , video_path, "--title", " \""+title+"\"", "--description" ," \""+ description+"\"", "--keywords" ," \""+keywords+ "\"", "--privacyStatus", privacy_status])
-        # print("youtube upload started")
--- Social_Manager/README.md
+++
@@ -1,7 +0,0 @@
-## These are few testing and execution samples of application
-
-![alt text](image.png)
-![alt text](image-1.png)
-![alt text](image-2.png)
-![alt text](image-3.png)
-![alt text](image-4.png)
